57da0ebfddcb1dde4688d3f5625a53bdba7ce510
MartinGUUS <martin.acosta.ro@gmail.com>
2026-03-28 10:04:26 -0600

    Subida V1 proyecto

---
+++ sile-fiee/src/test/java/com/sile/sile_fiee/SileFieeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sile.sile_fiee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SileFieeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
